c79d98d5167a7c3061ba69db8acd41468efb5468
DevMaurizio <maurizio.vdwerf@netvisit.nl>
2025-02-24 11:59:33 +0100

    add check on welcome and input text

--- Day-1/Project/BandNameGenerator.py
+++ Day-1/Project/BandNameGenerator.py
@@ -1,3 +1,4 @@
 class Bandname_Generator:
     def start(self):
-        print("Bandname generator")
+        print("Welcome to the Band Name Generator.")
+        input("What's the name of the city you grew up in?")
--- Day-1/Project/test_bandnamegenerator.py
+++ Day-1/Project/test_bandnamegenerator.py
@@ -1,9 +1,17 @@
 from BandNameGenerator import Bandname_Generator
 
 
-def test_outPut(capsys):
+def test_outputWelcometest(capsys):
     band_name_generator = Bandname_Generator()
     band_name_generator.start()
 
     captured = capsys.readouterr()
-    assert captured.out == "Bandname generator\n"
+    assert captured.out == "Welcome to the Band Name Generator.\n"
+
+
+def test_inputWithQuestionText(capsys):
+    band_name_generator = Bandname_Generator()
+    band_name_generator.start()
+
+    captured = capsys.readouterr()
+    assert captured.out == "What's the name of the city ou grew in?\n"
