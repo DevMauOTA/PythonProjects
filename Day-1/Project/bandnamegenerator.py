2261a7a3cb07983f9c59fc0217d076514744026b
DevMaurizio <maurizio.vdwerf@netvisit.nl>
2025-02-26 12:02:11 +0100

    day-1 finnish project

--- Day-1/Project/bandnamegenerator.py
+++ Day-1/Project/bandnamegenerator.py
@@ -1,3 +1,4 @@
-class BandNameGenerator:
-    def run(self):
-        print("Welcome to the Band Name Generator.")
+print("Welcome to the Band Name Generator.")
+input_city = input("What's the name of the city you grew up in?\n")
+input_pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + input_city + " " + input_pet_name)
